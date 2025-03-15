0e28dc362109b3fb432918e4a43fba002685b947
MaRkAnDrei11 <mpsanmiguel@paterostechnologicalcollege.edu.ph>
2025-03-15 14:51:32 +0800

    Update index.java

--- index.java
+++ index.java
@@ -1,4 +1,4 @@
-// Group 6: Max (Members: Jardeleza, San Miguel, Gabrillo, Santander, Muyrong, Antonio, Ordinario, Abohan,Tuazon)
+// Group 6: Max (Members: Jardeleza, San Miguel, Gabrillo, Santander, Muyrong, Antonio, Ordinario, Abohan)
 
 public static int multiply(int a, int a) {
     return a * a;
